273520848a1b56c68b3e4b715d611dbd2ab6e611
Your Name <you@example.com>
2025-11-30 05:08:42 +0800

    update test

---
+++ test.js
@@ -0,0 +1,8 @@
+const csskin_gg = require("./utils/csskin_gg")
+
+const test = async ()=>
+{
+    await csskin_gg.fetchSkinData()
+}
+
+test()
--- utils/csskin_gg.js
+++ utils/csskin_gg.js
@@ -1,6 +1,7 @@
 const path = require('path');
 //Chrome
 const puppeteer = require("puppeteer-core");
+const { MongoTable } = require('../db/db');
 const tables = {
     skins: new MongoTable("skins"),
     skins_batch: new MongoTable("skins_batch"),
@@ -8,7 +9,7 @@ const tables = {
     markets_batch: new MongoTable("markets_batch"),
 };
 const cases = require("../config/case.json")
-const markets = require("../config/market.json")
+const markets = require("../config/market.json");
 
 const chromePath = "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe";
 function sleep(ms) {
@@ -334,3 +335,7 @@ const casesData  =async (batchId,skin,url) => {
 
 // fetchMarketData()
 // fetchSkinData()
+module.exports = {
+  fetchMarketData,
+  fetchSkinData
+}

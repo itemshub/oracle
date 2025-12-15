ba5a13d17ae2ec26544dde81cbb8027db46a544a
Your Name <you@example.com>
2025-12-16 06:10:53 +0800

    update class for different cex

---
+++ class/c5/index.js
@@ -0,0 +1,159 @@
+// c5_class.js
+const fetch = require('node-fetch');
+
+class c5_class {
+  /**
+   * @param {Object} options
+   * @param {string} options.apiKey - C5GAME OpenAPI 的 app-key
+   * @param {string} [options.baseURL] - API 基础域名
+   */
+  constructor({ apiKey, baseURL = 'https://openapi.c5game.com' } = {}) {
+    this.config = {
+      apiKey,
+      baseURL,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    };
+  }
+
+  /** 修改 config */
+  setConfig(options) {
+    this.config = {
+      ...this.config,
+      ...options,
+      headers: {
+        ...this.config.headers,
+        ...(options.headers || {}),
+      },
+    };
+  }
+
+  /** 核心 fetch 请求封装 */
+  async request(path, method = 'GET', body = null, query = {}) {
+    // 必带 app-key
+    const url = new URL(`${this.config.baseURL}${path}`);
+    url.searchParams.set('app-key', this.config.apiKey);
+
+    // 附加额外 query
+    for (const key in query) {
+      if (query[key] !== undefined && query[key] !== null) {
+        url.searchParams.set(key, query[key]);
+      }
+    }
+
+    const opts = {
+      method,
+      headers: this.config.headers,
+    };
+
+    if (body) {
+      opts.body = JSON.stringify(body);
+    }
+
+    const res = await fetch(url.toString(), opts);
+    const json = await res.json();
+    return json;
+  }
+
+  /** 🧾 查询账号余额 GET /merchant/account/v1/balance */
+  async getBalance() {
+    return this.request('/merchant/account/v1/balance', 'GET');
+  }
+
+  /** 👤 查询用户 steam 信息 GET /merchant/account/v1/steamInfo */
+  async getSteamInfo() {
+    return this.request('/merchant/account/v1/steamInfo', 'GET');
+  }
+
+  /** 📜 在售列表 GET /merchant/sale/v1/search */
+  async searchSaleList(query = {}) {
+    return this.request('/merchant/sale/v1/search', 'GET', null, query);
+  }
+
+  /** 📦 在售饰品改价 POST /merchant/sale/v1/modify */
+  async modifySale(body) {
+    return this.request('/merchant/sale/v1/modify', 'POST', body);
+  }
+
+  /** 📤 下架在售饰品 POST /merchant/sale/v1/offsale */
+  async offSale(body) {
+    return this.request('/merchant/sale/v1/offsale', 'POST', body);
+  }
+
+  /** 📦 发起求购 POST /merchant/purchase/v1/create */
+  async createPurchase(body) {
+    return this.request('/merchant/purchase/v1/create', 'POST', body);
+  }
+
+  /** ❌ 取消求购 POST /merchant/purchase/v1/cancel */
+  async cancelPurchase(body) {
+    return this.request('/merchant/purchase/v1/cancel', 'POST', body);
+  }
+
+  /** 📊 求购列表 GET /merchant/purchase/v1/list */
+  async listPurchases(query = {}) {
+    return this.request('/merchant/purchase/v1/list', 'GET', null, query);
+  }
+
+  /** 📜 求购详情 GET /merchant/purchase/v1/detail */
+  async getPurchaseDetail(query = {}) {
+    return this.request('/merchant/purchase/v1/detail', 'GET', null, query);
+  }
+
+  /** 🏷 求购最高价 GET /merchant/purchase/v1/top */
+  async getPurchaseTop(query = {}) {
+    return this.request('/merchant/purchase/v1/top', 'GET', null, query);
+  }
+
+  /** 🧾 卖家订单列表 GET /merchant/order/v1/list */
+  async listSellerOrders(query = {}) {
+    return this.request('/merchant/order/v1/list', 'GET', null, query);
+  }
+
+  /** 🚚 发货接口 POST /merchant/order/v1/deliver */
+  async deliverOrder(body) {
+    return this.request('/merchant/order/v1/deliver', 'POST', body);
+  }
+
+  /** 📌 买家订单状态列表 POST /merchant/order/v2/buyer/status */
+  async buyerOrderStatus(body) {
+    return this.request('/merchant/order/v2/buyer/status', 'POST', body);
+  }
+
+  /** 📄 订单详情 GET /merchant/order/v2/buy/detail */
+  async getOrderDetail(query = {}) {
+    return this.request('/merchant/order/v2/buy/detail', 'GET', null, query);
+  }
+
+  /** 🛒 普通购买 POST /merchant/trade/v2/normal-buy */
+  async normalBuy(body) {
+    return this.request('/merchant/trade/v2/normal-buy', 'POST', body);
+  }
+
+  /** ⚡ 快速购买 POST /merchant/trade/v2/quick-buy */
+  async quickBuy(body) {
+    return this.request('/merchant/trade/v2/quick-buy', 'POST', body);
+  }
+
+  /** 📊 价格查询（Dev/未完全定义） GET /price/info */
+  async getPriceInfo(query = {}) {
+    return this.request('/price/info', 'GET', null, query);
+  }
+
+  /** 标准接口集 */
+
+  async balance()
+  {
+    try{
+        const bal = await this.getBalance();
+        return Number(bal.data?.balance)/7; //Value in USD
+    }catch(e)
+    {
+        console.error(e)
+        return 0;
+    }
+  }
+}
+
+module.exports = c5_class;
--- test.js
+++ test.js
@@ -2,9 +2,9 @@
 
 // const db =require("./db/db")
 
-const markets = require("./config/market.json")
+// const markets = require("./config/market.json")
 
-const cases = require("./config/case.json")
+// const cases = require("./config/case.json")
 
 // const igxeId = require("./utils/igxe/igxe.json")
 
@@ -13,48 +13,58 @@ const cases = require("./config/case.json")
 // const c5 = require("./utils/c5/index")
 // const uuyp = require("./utils/uupy/index")
 // const igxe = require("./utils/igxe/index")
+// const test = async ()=>
+// {
+//     // await csskin_gg.fetchSkinData()
+//     // await db.newAnnouncement(
+//     //     "平台构建中",
+//     //     "Itemshub 饰集小程序即将完成构建。TARO真不是人用的玩意儿。"
+//     // )
+
+//     // for(let i of markets)
+//     // {
+//     //     let logo = i.logo_url.split("/")
+//     //     i.logo_url = `https://itemshub-res.sidcloud.cn/markets/${logo[logo.length-1]}`
+//     // }
+//     // console.log(JSON.stringify(markets))
+//     for(let i of cases)
+//     {
+//         // i['item_id'] = buff[i.name]? buff[i.name] : 0
+//         // if(!i?.c5_id)
+//         // {
+//         //     i['c5_id'] = 100000000
+//         // }
+
+//         // if(!i?.uuyp_id)
+//         // {
+//         //     i['uuyp_id'] = 100000000
+//         // }
+
+//         // i['igxe_id'] = igxeId[i.name]? igxeId[i.name] : 0
+//         i.name = `${i.name} 特别款实体贴纸`
+//         i.img_url = "http://itemshub-res.sidcloud.cn/sticker.png"
+//     }
+//     console.log(JSON.stringify(cases))
+
+//     // console.log(await buff.price(cases[0]))
+
+//     // console.log(await csgo_market.price(cases[0]))
+
+//     // console.log(await c5.price(cases[0]))
+
+//     // console.log(await uuyp.price(cases[0]))
+
+//     // console.log(await igxe.price(cases[0]))
+// }
+
+
+const c5_class = require("./class/c5/index")
+const market_csgo_class = require("./class/market_csgo/index")
+const igxe_class = require("./class/igxe");
+const buff_class = require("./class/buff/index")
+const uupy_class = require("./class/uuyp/index")
 const test = async ()=>
 {
-    // await csskin_gg.fetchSkinData()
-    // await db.newAnnouncement(
-    //     "平台构建中",
-    //     "Itemshub 饰集小程序即将完成构建。TARO真不是人用的玩意儿。"
-    // )
-
-    // for(let i of markets)
-    // {
-    //     let logo = i.logo_url.split("/")
-    //     i.logo_url = `https://itemshub-res.sidcloud.cn/markets/${logo[logo.length-1]}`
-    // }
-    // console.log(JSON.stringify(markets))
-    for(let i of cases)
-    {
-        // i['item_id'] = buff[i.name]? buff[i.name] : 0
-        // if(!i?.c5_id)
-        // {
-        //     i['c5_id'] = 100000000
-        // }
-
-        // if(!i?.uuyp_id)
-        // {
-        //     i['uuyp_id'] = 100000000
-        // }
-
-        // i['igxe_id'] = igxeId[i.name]? igxeId[i.name] : 0
-        i.name = `${i.name} 特别款实体贴纸`
-        i.img_url = "http://itemshub-res.sidcloud.cn/sticker.png"
-    }
-    console.log(JSON.stringify(cases))
-
-    // console.log(await buff.price(cases[0]))
-
-    // console.log(await csgo_market.price(cases[0]))
-
-    // console.log(await c5.price(cases[0]))
-
-    // console.log(await uuyp.price(cases[0]))
-
-    // console.log(await igxe.price(cases[0]))
 }
 
 test()

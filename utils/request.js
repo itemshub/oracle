b76f9f64ad3b862742c8436530d85c50df53943a
Your Name <you@example.com>
2025-12-16 02:47:08 +0800

    update csgomarket api

--- utils/request.js
+++ utils/request.js
@@ -17,28 +17,33 @@ async function doRequest(options)
 }
 
 require('dotenv').config()
+const base_url ={
+    market:"https://market.csgo.com/api",
+    buff:"https://buff.163.com/api",
+    igxe:"https://www.igxe.cn"
+}
 const router = {
     buff:{
-        price:"https://buff.163.com/api/market/goods/sell_order?game=csgo&page_num=1&sort_by=default&mode=&allow_tradable_cooldown=1&goods_id=",
-        buy_order:"https://buff.163.com/api/market/goods/buy_order?game=csgo&page_num=1&goods_id="
+        price:`${base_url.buff}/market/goods/sell_order?game=csgo&page_num=1&sort_by=default&mode=&allow_tradable_cooldown=1&goods_id=`,
+        buy_order:`${base_url.buff}/market/goods/buy_order?game=csgo&page_num=1&goods_id=`
     },
     igxe:{
-        price:"https://www.igxe.cn/product/trade/730/",
-        buy_order:"https://www.igxe.cn/purchase/get_product_purchases?product_id="
+        price:`${base_url.igxe}/product/trade/730/`,
+        buy_order:`${base_url.igxe}/purchase/get_product_purchases?product_id=`
     },
     market:{
-        order_usd:"https://market.csgo.com/api/v2/prices/orders/USD.json",
-        price_usd:"https://market.csgo.com/api/v2/prices/USD.json",
-        buy:`https://market.csgo.com/api/v2/buy-for?key=${process.env.MARKET_KEY}&${process.env.RECIVER_STEAM}`
+        price_usd:`${base_url.market}/v2/prices/USD.json`,
+        buy:`${base_url.market}/v2/buy?key=${process.env.MARKET_KEY}`,
+        buy_for:`${base_url.market}/v2/buy-for?key=${process.env.MARKET_KEY}&${process.env.RECIVER_STEAM}`,
+        sell:`${base_url.market}/v2/add-to-sale?key=${process.env.MARKET_KEY}`,
+        sell_remove_all:`${base_url.market}/v2/remove-all-from-sale?key=${process.env.MARKET_KEY}`,
+        balance:`${base_url.market}/v2/get-money?key=${process.env.MARKET_KEY}`
     }
 }
 async function buff_price(id) {
     var options = {
         'method': 'GET',
         'url': router.buff.price+id,
-        // 'headers': {
-        //     'Cookie': cookies
-        // },
     };
     return doRequest(options);
 }
@@ -47,9 +52,6 @@ async function buff_buy_order(id) {
     var options = {
         'method': 'GET',
         'url': router.buff.buy_order+id,
-        // 'headers': {
-        //     'Cookie': cookies
-        // },
     };
     return doRequest(options);
 }
@@ -96,12 +98,36 @@ async function market_order_usd() {
 
 async function market_buy(name,price)
 {
-    // console.log(router.market.buy)
     var options = {
         'method': 'GET',
         'url': encodeURI(router.market.buy+`&hash_name=${name}&price=${price}`),
     };
-    // console.log(options)
+    return doRequest(options);
+}
+
+async function market_sell(id,price)
+{
+    var options = {
+        'method': 'GET',
+        'url': encodeURI(router.market.sell+`&id=${name}&price=${price}&cur=USD`),
+    };
+    return doRequest(options);
+}
+
+async function market_remove_all(id,price)
+{
+    var options = {
+        'method': 'GET',
+        'url': encodeURI(router.market.sell_remove_all),
+    };
+    return doRequest(options);
+}
+async function market_balance(name,price)
+{
+    var options = {
+        'method': 'GET',
+        'url': encodeURI(router.market.balance),
+    };
     return doRequest(options);
 }
 
@@ -115,12 +141,21 @@ async function getIP()
     return doRequest(options);
 }
 module.exports = {
-    buff_price,
-    buff_buy_order,
-    igxe_price,
-    igxe_buy_order,
+    getIP,
+
+    //Market.csgo interfaces : 
+    market_buy,
+    market_sell,
+    market_remove_all,
+    market_balance,
     market_price_usd,
     market_order_usd,
-    market_buy,
-    getIP
+
+    //BUFF interfaces :
+    buff_price,
+    buff_buy_order,
+
+    //IGXE interfaces :
+    igxe_price,
+    igxe_buy_order,
 }
